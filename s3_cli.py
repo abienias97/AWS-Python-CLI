0d522365a25da7969e777416e325cf62d6289a89
abienias97 <wonszarek@gmail.com>
2024-10-18 10:50:54 +0200

    create file listing feature

--- s3_cli.py
+++ s3_cli.py
@@ -1,6 +1,8 @@
 import os
+import re
 import argparse
 import boto3
+from botocore.exceptions import NoCredentialsError
 from dotenv import load_dotenv
 
 # Load environment variables
@@ -9,6 +11,26 @@ load_dotenv()
 # AWS credentials and config
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-AWS_REGION = os.getenv('AWS_REGION')
+AWS_REGION = os.getenv('AWS_REGION') # No region was specified?
 S3_BUCKET_NAME = os.getenv('S3_BUCKET_NAME')
 S3_PREFIX = os.getenv('S3_PREFIX')
+
+s3 = boto3.client(
+    's3',
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    region_name=AWS_REGION
+)
+
+def list_files():
+    try:
+        response = s3.list_objects_v2(Bucket=S3_BUCKET_NAME, Prefix=S3_PREFIX)
+        if 'Contents' in response:
+            for obj in response['Contents']:
+                print(obj['Key'])
+        else:
+            print("No files found.")
+    except NoCredentialsError:
+        print("Credentials not available.")
+        
+list_files()
